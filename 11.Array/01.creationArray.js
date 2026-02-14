75482651a281042e3b6fb4ede30ead9bfcc306b9
NandaBendkoli <bendkolinanda12@gmail.com>
2026-02-14 10:06:11 +0530

    added creationArrays.js file

---
+++ 11.Array/01.creationArray.js
@@ -0,0 +1,19 @@
+
+// array is special variable that can hold multiple values in single place 
+
+// creating array
+// 1. using squre bracket
+let arr = [1,2,3,4,5];
+
+// using a array constructor (new Keyword)
+
+let arr2 = new Array(1,2,3,4,5);
+// but it is not recommended because it behavees weirdly with one number
+
+let arr3 = new Array(3); 
+// this will create an aaray of length 3 with undefined value
+// arr3 =["undefined","undefined","undefined"]
+// this does not create an array with single value which is arr3 = [3];
+
+// thats why the best way to createan array is using square bracket
+
